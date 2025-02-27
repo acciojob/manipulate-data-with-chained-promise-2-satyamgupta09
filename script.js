3f6c2506c808583b7108fb57897d347950fd6f96
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2025-02-27 22:18:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-//your JS code here. If required.
+function intaliseArray() {
+	return new Promise((resolve)=>{
+		setTimeout(()=>{
+			resolve([1,2,3,4]);
+		},3000)
+	});
+}
+
+function evenNumber(arr) {
+	return new Promise((resolve)=>{
+		setTimeout(()=>{
+			const even = arr.filter((num)=>num%2==0);
+			document.getElementById("output").innerHTML+=even.map(x=>`<p>${x}</p>`);
+			resolve(even);
+		},1000)
+	})
+}
+
+function multiplyNumber(arr) {
+	return new Promise((resolve)=>{
+		setTimeout(()=>{
+			// document.getElementById("output").innerHTML+="";
+			let doubled = arr.map(x=>x*2);
+			document.getElementById("output").innerHTML+=doubled.map(x => `<p>${x}</p>`);
+			resolve(doubled);
+		},2000)
+	})
+}
+
+function helper() {
+	intaliseArray().then(evenNumber).then(multiplyNumber);
+}
+
+window.onload = helper;
