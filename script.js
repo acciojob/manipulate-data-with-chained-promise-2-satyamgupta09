01380519565c71369f3715ca53c3b4670e9de2ee
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2025-04-06 00:38:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,34 @@
 function prom1(){
-	return new Promise((resolve)=>{
+	return new Promise((resolve, reject)=>{
 		setTimeout(()=>{
-			resolve([1,2,3,4])
-		},3000)
+			resolve([1, 2, 3, 4]);
+		},3000);
 	})
 }
 
 function prom2(arr){
-	return new Promise((resolve)=>{
+	return new Promise((resolve, reject)=>{
 		setTimeout(()=>{
-			let newArr = arr.filter((item)=>item%2==0);
+			let newArr = arr.filter((num)=>num%2==0);
 			resolve(newArr);
-		},1000)
+		},1000);
 	})
 }
 
 function prom3(arr){
-	return new Promise((resolve)=>{
+	return new Promise((resolve, reject)=>{
 		setTimeout(()=>{
-			let newArr = arr.map((item)=>item*2);
+			let newArr = arr.map((num)=>num*2);
 			resolve(newArr);
-		},3000)
+		},2000);
 	})
 }
 
-prom1().then(res=>{return prom2(res)})
-	.then(res=>{
-	document.getElementById('output').innerHTML=res.map((item)=>`<p>${item}</p>`);
-	return prom3(res);
-}).then(data=>{
+prom1().then((data)=> return prom2(data))
+.then((data)=> 
+	document.getElementById('output').innerHTML=data.map((item)=>`<p>${item}</p>`);
+	return prom3(data))
+.then((data)=> 
 	document.getElementById('output').innerHTML=data.map((item)=>`<p>${item}</p>`)
-})
-
+);
 
