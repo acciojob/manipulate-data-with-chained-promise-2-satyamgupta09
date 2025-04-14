b749c03e8dff44fbc135d203efe6632f45c00c83
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2025-04-14 23:30:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,71 @@
-
 function prom1(){
-	return new Promise((resolve)=>{
-		setTimeout(()=>{
-			resolve([1,2,3,4])
-		},3000)
-	})
+	return new Promise((resolve, reject) => {
+	setTimeout(()=>{
+		resolve([1, 2, 3, 4]);
+	},3000);
+})
 }
 
-
-function prom2(arr){
-	return new Promise((resolve)=>{
-		setTimeout(()=>{
-			let newArr = arr.filter((item)=>item%2==0);
-			resolve(newArr);
-		},1000)
-	})
+function prom2(arr) {
+	return new Promise((resolve, reject) => {
+	setTimeout(()=>{
+		let newArr = arr.filter(item => item%2==0);
+		resolve(newArr);
+	},1000);
+})
 }
 
 function prom3(arr){
-	return new Promise((resolve)=>{
+	return new Promise((resolve, reject) => {
 		setTimeout(()=>{
-			let newArr = arr.map((item)=>item*2);
+			let newArr = arr.map(item => item*2); 
 			resolve(newArr);
-		},3000)
+		},2000);
 	})
 }
 
-prom1().then(res=>{return prom2(res)})
-	.then(res=>{
-	document.getElementById('output').innerHTML=res.map((item)=>`<p>${item}</p>`);
-	return prom3(res);
-}).then(data=>{
-	document.getElementById('output').innerHTML=data.map((item)=>`<p>${item}</p>`)
+prom1().then(data =>{
+	return prom2(data);
+}).then(data => {
+	document.getElementById('output').innerHTML = data.map(item => `<p>${item}</p>`);
+	return prom3(data);
+}).then(data => {
+	document.getElementById('output').innerHTML = data.map(item => `<p>${item}</p>`)
 })
+
+
+
+// function prom1(){
+// 	return new Promise((resolve)=>{
+// 		setTimeout(()=>{
+// 			resolve([1,2,3,4])
+// 		},3000)
+// 	})
+// }
+
+
+// function prom2(arr){
+// 	return new Promise((resolve)=>{
+// 		setTimeout(()=>{
+// 			let newArr = arr.filter((item)=>item%2==0);
+// 			resolve(newArr);
+// 		},1000)
+// 	})
+// }
+
+// function prom3(arr){
+// 	return new Promise((resolve)=>{
+// 		setTimeout(()=>{
+// 			let newArr = arr.map((item)=>item*2);
+// 			resolve(newArr);
+// 		},3000)
+// 	})
+// }
+
+// prom1().then(res=>{return prom2(res)})
+// 	.then(res=>{
+// 	document.getElementById('output').innerHTML=res.map((item)=>`<p>${item}</p>`);
+// 	return prom3(res);
+// }).then(data=>{
+// 	document.getElementById('output').innerHTML=data.map((item)=>`<p>${item}</p>`)
+// })
